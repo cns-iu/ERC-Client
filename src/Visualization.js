d6169dc79b6c58ee873a14f8c307c6f3a99eb4b9
Gagandeep Singh <whg_27@yahoo.com>
2017-09-23 17:38:26 +1000

    scimap zoom test 5

--- src/Visualization.js
+++ src/Visualization.js
@@ -99,7 +99,7 @@ var Visualization = function(scope) {
                         zoomtext.text("(" + Utilities.round(scope.zoom.scale(), 2) + "x)");
                     }
                     else {
-                         scope.SVG.selectAll("underlyingNodes").attr("transform", "scale(" + scope.zoom.scale() + ")");
+                         scope.SVG.underlyingNodeG.attr("transform", "translate(" + scope.zoom.translate() + ")scale(" + scope.zoom.scale() + ")");
                          zoomtext.text("(" + Utilities.round(scope.zoom.scale(), 2) + "x)");
                     }
 
