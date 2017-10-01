b91e122e53a60ed64b9b8681631aefdd60105f64
singhgag <singhgag@indiana.edu>
2017-10-01 00:52:42 -0400

    Zoom level change

--- visuals/D3ProportionalSymbol/D3ProportionalSymbol/D3ProportionalSymbol.js
+++ visuals/D3ProportionalSymbol/D3ProportionalSymbol/D3ProportionalSymbol.js
@@ -139,7 +139,7 @@ var div = d3.select("body").append("div")
   // Set leaflet map
    context.map = new L.map('map', {
     center: new L.LatLng(50,15),
-    zoom: 3,
+    zoom: 4,
     layers: [
     L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
